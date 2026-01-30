5d65787ca6abf460b554ea88de62eb802a69d3a2
ViCor44 <victor.a.correia@gmail.com>
2026-01-30 14:31:12 +0000

    remap

--- systems_map.php
+++ systems_map.php
@@ -2,7 +2,7 @@
 return [
     'sae' => [
         'name'        => 'SAE - Sistema de Apoio à Enfermaria',
-        'login_url'   => 'http://localhost/enfermaria/public/index.php?route=sso',
+        'login_url'   => 'http://191.188.126.13/enfermaria/public/index.php?route=sso',
         'dsn'         => 'mysql:host=localhost;dbname=enfermaria;charset=utf8mb4',
         'db_user'     => 'root',
         'db_pass'     => '',
@@ -12,7 +12,7 @@ return [
 
     'crewgest' => [
         'name'        => 'CrewGest - Gestão de Fardas',
-        'login_url'   => 'http://localhost/economato/sso_login.php',
+        'login_url'   => 'http://191.188.126.13/economato/sso_login.php',
         'dsn'         => 'mysql:host=localhost;dbname=econo_app;charset=utf8mb4',
         'db_user'     => 'root',
         'db_pass'     => '',
@@ -22,7 +22,7 @@ return [
 
     'worklog' => [
         'name'        => 'WorkLog - CMMS',
-        'login_url'   => 'http://localhost/work_log/sso_login.php',
+        'login_url'   => 'http://191.188.126.13/work_log/sso_login.php',
         'dsn'         => 'mysql:host=localhost;dbname=cmms;charset=utf8mb4',
         'db_user'     => 'root',
         'db_pass'     => '',
